f32c076cec9e29f34e0440b2f50d08344a545234
Alexandru Popa <alex.popa@stessa.com>
2022-04-07 12:16:33 +0300

    use effect tutorial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 // import StateTutorial from "./useState/StateTutorial";
-import ReducerTutorial from './useReducer/ReducerTutorial';
+// import ReducerTutorial from './useReducer/ReducerTutorial';
+import EffectTutorial from './useEffect/EffectTutorial';
 
 function App() {
   return (
     <div className="App">
-      <ReducerTutorial />
+      <EffectTutorial />
     </div>
   );
 }
